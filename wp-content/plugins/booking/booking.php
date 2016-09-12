8a40f73fdacef00df4fc2e2d64d9df1de639d6a6
Daniel Roth <danielroth@BBug.local>
2016-09-12 02:36:51 +0200

    Edit calendar

--- wp-content/plugins/booking/booking.php
+++ wp-content/plugins/booking/booking.php
@@ -25,7 +25,7 @@ function booking_form_handler() {
 	$html_form .= "</p></div>";	
 
 	$register_form = "<div id='div-register-form>'";
-	$register_form .= "<h1>Register New Client</h1>";
+	$register_form .= "<h2>Register New Client</h2>";
 	$register_form .= "<table><tr>";
 	$register_form .= "<td>First Name</td>";
 	$register_form .= "<td><input type=text /></td></tr>";
@@ -34,15 +34,35 @@ function booking_form_handler() {
 	$register_form .= "<tr><td>Birth of Date</td>";
 	$register_form .= "<td>calendar</td></tr>";
 	$register_form .= "</table>";
-	$register_form .= "<button>Submit</button></div>";
+	$register_form .= "<button id='btn-submit-client'>Submit</button></div>";
 
-	$html_form .= $register_form;
+	$booking_form = "<div id='div-booking-form'>";
+	$booking_form .= "<h2>Booking an Appointment</h2>";
+	$booking_form .= "<table><tr>";
+	$booking_form .= "<td>Doctor</td>";
+	$booking_form .= "<td><select id='select-doctor'><option></option></select></td></tr>";
+	$booking_form .= "<td>Location</td>";
+	$booking_form .= "<td><select id='select-location'><option></option></select></td></tr>";
+	$booking_form .= "<tr><td>Date</td>";
+	$booking_form .= "<td><input type=text class='form-control' id='demo' /></td></tr>";
+	$booking_form .= "<tr><td>Time</td>";
+	$booking_form .= "<td><input type=text class='DatePicker' /></td></tr>";
+	$booking_form .= "</table>";
+	$booking_form .= "<button id='btn-submit-booking'>Submit</button></div>";
+
+	$html_form .= $register_form . $booking_form;
 
 	return $html_form;
 }
 
+add_action( 'wp_enqueue_scripts', 'booking_custom_assets' );
 add_action( 'wp_footer', 'booking_custom_jscript' );
 
+function booking_custom_assets() {
+	wp_enqueue_style( 'calendar-style', BOOKING_PLUGIN_URL . '/styles/dcalendar.picker.css' );
+}
+
 function booking_custom_jscript() {
+    wp_enqueue_script( 'calendar-script', BOOKING_PLUGIN_URL . '/scripts/dcalendar.picker.js' );
     wp_enqueue_script( 'booking-script', BOOKING_PLUGIN_URL . '/scripts/script.js' );
 }
--- wp-content/plugins/booking/scripts/script.js
+++ wp-content/plugins/booking/scripts/script.js
@@ -3,15 +3,16 @@ jQuery( function ( $ )
 	'use strict';
 	
 	$(document).ready( function() {
-		$(document).html('sdfsdfs');
+		
+		// $.ajax({
+		//   type: "POST",
+		//   url: "https://www.google.co.uk",
+		//   data: "data",
+		//   success: function(result){
+		//         alert(result);
+		//     }
+		// });
 
-		$.ajax({
-		  type: "POST",
-		  url: "https://www.google.co.uk",
-		  data: "data",
-		  success: function(result){
-		        alert(result);
-		    }
-		});
+		$("input#demo").dcalendarpicker();
 	})
 } );
