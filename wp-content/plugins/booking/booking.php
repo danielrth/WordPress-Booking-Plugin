75d0b8cfe8e81b0925a3c6b5d9571ef234b32d21
Daniel Roth <danielroth@BBug.local>
2016-09-22 14:11:00 +0200

    Module to generate JwToken

--- wp-content/plugins/booking/booking.php
+++ wp-content/plugins/booking/booking.php
@@ -14,11 +14,9 @@ Author URI:
 */
 
 define( 'BOOKING_PLUGIN_URL', plugins_url( '', __FILE__ ));
-
-
-/*function action_callback_coreplus() {
-	echo "sdfsdfsdfsadfsdfsadfasdfsad";
-}*/
+define( 'COREPLUS_CONSUMER_ID', "353891d3-a187-4d1a-a6cb-87ebc2f6db26");
+define( 'COREPLUS_API_ACCESS_TOKEN', "6176dca0-8be2-496c-9e47-cc4ed06fd7b2");
+define( 'COREPLUS_SECRET_KEY', "1kUwumuhkadPEWfKlgVH/3cUGM+DL4zNfw7YXwUm2zYednvAM8P8QQ==");
 
 function booking_custom_assets() {
 	wp_enqueue_style( 'calendar-style', BOOKING_PLUGIN_URL . '/styles/dcalendar.picker.css' );
@@ -32,9 +30,6 @@ function booking_custom_jscript() {
 add_shortcode( 'booking_form', 'booking_form_handler' );
 
 function booking_form_handler() {
-
-	// add_action('wp_ajax_nopriv_my_action', 'action_callback_coreplus');
-	// add_action('wp_ajax_my_action', 'action_callback_coreplus');
 
 	add_action( 'wp_enqueue_scripts', 'booking_custom_assets');
 	add_action( 'wp_footer', 'booking_custom_jscript' );
@@ -70,47 +65,53 @@ function booking_form_handler() {
 	$booking_form .= "</table>";
 	$booking_form .= "<button id='btn-submit-booking'>Submit</button></div>";
 
-	$html_form .= $register_form . $booking_form;
+	$html_form .= $register_form . $booking_form . $auth;
 
 	return $html_form;
 }
 
+add_action( 'wp_ajax_action_coreplus_api', 'ajax_call_coreplus' );
+// add_action( 'wp_ajax_nopriv_action_coreplus_api', 'ajax_call_coreplus' );
 
-
-add_action( 'wp_ajax_action1', 'prefix_ajax_add_foobar' );
-add_action( 'wp_ajax_nopriv_action1', 'prefix_ajax_add_foobar' );
-
-function prefix_ajax_add_foobar() {
-	$ca = plugin_dir_path( __FILE__ ) . 'cacert.pem';
+function ajax_call_coreplus() {
 	
 	$curl = curl_init();
 	// Set some options - we are passing in a useragent too here
-	curl_setopt($curl, CURLOPT_URL, 'https://staging.coreplus.com.au/API/Core/v2/location/');	
+	curl_setopt($curl, CURLOPT_URL, 'https://sandbox.coreplus.com.au/API/Core/v2/'.$_POST['api_name'].'/');	
 	curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-	// curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
-	// curl_setopt($curl, CURLOPT_SSL_VERIFYHOST,  FALSE);
-	// curl_setopt($curl, CURLOPT_SSL_VERSION,  3);
-	curl_setopt ($curl, CURLOPT_CAINFO, $ca ) ;
+	curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
 
-	//curl_setopt($curl, CURLOPT_HTTPHEADER, array(
-    //	'Authorization: JwToken eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJodHRwOi8vbG9jYWxob3N0IiwiYXVkIjoiaHR0cHM6Ly9zdGFnaW5nLmNvcmVwbHVzLmNvbS5hdSIsIm5iZiI6MTQ3MzY4MjQ0NCwiZXhwIjoxNDczNzgyNDQ0LCJjb25zdW1lcklkIjoiMzUzODkxZDMtYTE4Ny00ZDFhLWE2Y2ItODdlYmMyZjZkYjI2IiwiYWNjZXNzVG9rZW4iOiI2MTc2ZGNhMC04YmUyLTQ5NmMtOWU0Ny1jYzRlZDA2ZmQ3YjIiLCJ1cmwiOiIvQVBJL0NvcmUvdjIvbG9jYXRpb24vIiwiaHR0cE1ldGhvZCI6IkdFVCJ9.SKJGvbkNrr2Pcsx1MVrxgsEbLjO2HJ9wAdqWb_MvAGo'
-    	// 'Authorization: JwToken eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJodHRwOi8vbG9jYWxob3N0IiwiYXVkIjoiaHR0cHM6Ly9zdGFnaW5nLmNvcmVwbHVzLmNvbS5hdSIsIm5iZiI6MTQ3MzY4MjQ0NCwiZXhwIjoxNDczNzgyNDQ0LCJjb25zdW1lcklkIjoiMzUzODkxZDMtYTE4Ny00ZDFhLWE2Y2ItODdlYmMyZjZkYjI2IiwiYWNjZXNzVG9rZW4iOiI2MTc2ZGNhMC04YmUyLTQ5NmMtOWU0Ny1jYzRlZDA2ZmQ3YjIiLCJ1cmwiOiIvQVBJL0NvcmUvdjIvbG9jYXRpb24vIiwiaHR0cE1ldGhvZCI6IkdFVCJ9.SKJGvbkNrr2Pcsx1MVrxgsEbLjO2HJ9wAdqWb_MvAGo'
-    //));
+	$jwToken = generateJwToken($_POST['api_name'], $_POST['type']);
+	curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+    	$jwToken
+    ));
 
-	// Send the request & save response to $resp
 	$resp = curl_exec($curl);
-	
-	// var_dump($resp);
-	// $version = curl_version();
-	// $ssl_supported= ($version['features'] & CURL_VERSION_SSL);
-	// var_dump($ssl_supported);
-
+	echo $resp;
 	if(curl_error($curl))
 	{
 	    echo 'error:' . curl_error($curl);
 	}
-
 	// Close request to clear up some resources
 	curl_close($curl);
 
+}
+
+function generateJwToken($apiName, $callType) {
+	require_once( plugin_dir_path() . 'jwt.php' );
+	
+	$auth = JWT::encode( array( 
+		  "iss" => "http://localhost",
+		  "aud" => "https://sandbox.coreplus.com.au",
+		  "nbf" => 1474534917,
+		  "exp" => 1483782444,
+		  "consumerId" => COREPLUS_CONSUMER_ID,
+		  "accessToken" => COREPLUS_API_ACCESS_TOKEN,
+		  "url" => "/API/Core/v2/" . $apiName . "/",
+		  "httpMethod" => $callType), COREPLUS_SECRET_KEY );
+
+	// $token = "";
+	/* $token = "Authorization: JwToken eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJodHRwOi8vbG9jYWxob3N0IiwiYXVkIjoiaHR0cHM6Ly9zdGFnaW5nLmNvcmVwbHVzLmNvbS5hdSIsIm5iZiI6MTQ3MzY4MjQ0NCwiZXhwIjoxNDczNzgyNDQ0LCJjb25zdW1lcklkIjoiMzUzODkxZDMtYTE4Ny00ZDFhLWE2Y2ItODdlYmMyZjZkYjI2IiwiYWNjZXNzVG9rZW4iOiI2MTc2ZGNhMC04YmUyLTQ5NmMtOWU0Ny1jYzRlZDA2ZmQ3YjIiLCJ1cmwiOiIvQVBJL0NvcmUvdjIvbG9jYXRpb24vIiwiaHR0cE1ldGhvZCI6IkdFVCJ9.SKJGvbkNrr2Pcsx1MVrxgsEbLjO2HJ9wAdqWb_MvAGo"; */
+
+	return "Authorization: JwToken " . $auth;
 }
--- wp-content/plugins/booking/scripts/script.js
+++ wp-content/plugins/booking/scripts/script.js
@@ -5,16 +5,96 @@ jQuery( function ( $ )
 	$(document).ready( function() {
 
 		jQuery.post(
-		    '/wp-admin/admin-ajax.php', 
+		    '/mywp/wp-admin/admin-ajax.php', 
 		    {
-		        'action': 'action1',
-		        'data':   'foobarid'
+		        'action': 'action_coreplus_api',
+		        'api_name':   'location',
+		        'type': 'GET'
 		    }, 
 		    function(response){
-		        alert('The server responded: ' + response);
+		    	response = response.slice(0, -1);
+
+		        var locations = jQuery.parseJSON(response);
+		        locations = locations.locations;
+		        
+		        var htmlSelectorLocations = "";
+		        for (var key in locations) {
+		        	htmlSelectorLocations += "<option>" + locations[key]['name'] + "</option>";
+		        }
+
+		        $('#select-location').html(htmlSelectorLocations);
 		    }
 		);
 
-		$("input#demo").dcalendarpicker();
-	})
+		jQuery.post(
+		    '/mywp/wp-admin/admin-ajax.php', 
+		    {
+		        'action': 'action_coreplus_api',
+		        'api_name':   'client',
+		        'type': 'GET'
+		    }, 
+		    function(response){
+		    	response = response.slice(0, -1);
+
+		        var clients = jQuery.parseJSON(response);
+		        clients = clients.clients;
+		        
+		        var htmlSelectorClients = "";
+		        for (var key in clients) {
+		        	htmlSelectorClients += "<option>" + clients[key]['firstName'] + "</option>";
+		        }
+
+		        $('#select-registered-clients').html(htmlSelectorClients);
+		    }
+		);
+
+		jQuery.post(
+		    '/mywp/wp-admin/admin-ajax.php', 
+		    {
+		        'action': 'action_coreplus_api',
+		        'api_name':   'client',
+		        'type': 'POST'
+		    }, 
+		    function(response){
+		    	response = response.slice(0, -1);
+
+		        var clients = jQuery.parseJSON(response);
+		        clients = clients.clients;
+		        
+		        var htmlSelectorClients = "";
+		        for (var key in clients) {
+		        	htmlSelectorClients += "<option>" + clients[key]['firstName'] + "</option>";
+		        }
+
+		        $('#select-registered-clients').html(htmlSelectorClients);
+		    }
+		);
+		{
+"firstName":"Phynx",
+"middleName": "Huntington", 
+"lastName":"Slavia",
+"gender":"Male",
+"dateOfBirth":"1961-07-30",
+"Email":"aaa@ggg.com",
+"phoneNumberMobile":"0334819194"
+}
+		// $("input#demo").dcalendarpicker();
+	});
+
+	function sendRequestToAction(action_name, api_name, request_type) {
+		jQuery.post(
+		    '/mywp/wp-admin/admin-ajax.php', 
+		    {
+		        'action': 'action_coreplus_api',
+		        'api_name':   'client',
+		        'type': 'POST'
+		    }, 
+		    function(response){
+		    	response = response.slice(0, -1);
+
+		        var resp_data = jQuery.parseJSON(response);
+		        
+		    }
+		);
+	}
 });
